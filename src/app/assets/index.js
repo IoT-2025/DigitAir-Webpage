3c974dc098eac1de19e7757237110ca993cc5f87
andrea <103574857+andrea-andrenucci@users.noreply.github.com>
2025-09-02 19:20:59 +0200

    Fix + installed sharp

--- src/app/assets/index.js
+++ src/app/assets/index.js
@@ -1,7 +1,7 @@
 import esp32 from './esp32.png'
 import mockup from './computer_mockup.png'
 import bg_anim from './home_bg.webp'
-import digitair_logo from './DigitAir.svg'
+import digitair_logo from './DigitAIR.svg'
 import vfx from "./bg_vfx.png";
 
 
